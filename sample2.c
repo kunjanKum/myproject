b43a442b54b3c9ca1872c7f9117de8b54282bb35
kunjanKum <strlnk523@gmail.com>
2020-02-24 03:42:54 +0000

    test c code

---
+++ sample2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+   printf("\n this is sample c code")
+   printf("\n this is sample c code")	
+   printf("\n this is sample c code") 
+   printf("\n this is sample c code") 
+   printf("\n this is sample c code") 
+
+    return 0
+}  
