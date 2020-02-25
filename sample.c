7827570749987747c169478b21d9f7d5b33571f6
kunjanKum <strlnk523@gmail.com>
2020-02-25 06:03:33 +0000

    sample c code

---
+++ sample.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("\ welcome to c programing")
+    printf("\ welcome to c programing")
+    printf("\ welcome to c programing")
+    printf("\ welcome to c programing")
+    
+    return 0
+}    
